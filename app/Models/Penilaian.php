d56fc6a7cfbbc6dd9be20be354b823f5248ba04b
Seti <setiheriyanto42@guru.smp.belajar.id>
2026-07-13 21:45:07 +0700

    Sprint 6 - Model Penilaian Sikap selesai

--- app/Models/Penilaian.php
+++ app/Models/Penilaian.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Penilaian extends Model
 {
-    //
+    protected $fillable = [
+
+        'sesi_mengajar_id',
+
+        'siswa_id',
+
+        'predikat',
+
+        'catatan',
+
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELASI
+    |--------------------------------------------------------------------------
+    */
+
+    public function sesiMengajar()
+    {
+        return $this->belongsTo(SesiMengajar::class);
+    }
+
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class);
+    }
 }
